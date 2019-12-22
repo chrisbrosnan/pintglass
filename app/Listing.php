87e5a9838885d41051eeaf0974165bb608e18b04
Christopher Brosnan <cbrosnan00@gmail.com>
2019-12-22 13:23:53 +0000

    Adding Laravel files

---
+++ app/Listing.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
+}
