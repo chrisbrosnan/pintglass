1ef181b2a53e970b3b7106a6ff450aaedbe9d709
Chris Brosnan <cbrosnan00@gmail.com>
2021-01-19 08:02:40 +0000

    Maintenance Page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        body 
+        {
+            background: url(kazuend-NmvMhov1sYc-unsplash.jpg); 
+            background-size: cover;
+            background-position: center top; 
+        }
+    </style>
+</head>
+<body>
+    <div class="col-12 col-md-8 mx-auto bg-white h-80 p-10">
+        <h1 class="mx-auto" style="margin: auto; font-size: 5em; width: 100%; text-align: center;">PintGlass LDN is under maintenance</h1>
+        <h2 style="margin: auto; font-size: 3em; width: 100%; text-align: center;">Check back again shortly</h2>
+    </div>
+<body>
+</html>
