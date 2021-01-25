17d203e56d710a54f6cc7147c26d38cf6786b987
Chris Brosnan <cbrosnan00@gmail.com>
2021-01-25 18:49:07 +0000

    refactor controllers

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,9 @@
 			<div class="col-12 col-md-10 m-5">
 				<div class="card">					
 					<div class="card-body row">
-						@php(print_r($beverageFeed))
+						{{ $beverageFeed }}
 						<br/><br/>
-						@php(print_r($blogFeed))
+						{{ $blogFeed }}
 					</div>
 				</div>
 			</div>
