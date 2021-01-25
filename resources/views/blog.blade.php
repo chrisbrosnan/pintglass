6bc410bf73447b331d0ec12c9a3134831cb0e707
Chris Brosnan <cbrosnan00@gmail.com>
2021-01-25 20:55:25 +0000

    Adding Global variables to app template

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -13,8 +13,7 @@
 						<div class="container">
 							<div class="row">
 								<div class="col-12 col-md-4">
-									<amp-img class="col-12" src="{{ URL::to('/') }}/{{ $blogData['image']['path'] }}" width="300" height="400"></amp-img>
-									
+
 									<div class="col-12 text-center mb-4 p-0 m-0">
 										<script type="text/javascript">amzn_assoc_ad_type ="responsive_search_widget"; amzn_assoc_tracking_id ="chrisbrosna0e-21"; amzn_assoc_marketplace ="amazon"; amzn_assoc_region ="GB"; amzn_assoc_placement =""; amzn_assoc_search_type = "search_widget";amzn_assoc_width ="300"; amzn_assoc_height ="300"; amzn_assoc_default_search_category =""; amzn_assoc_default_search_key ="{{ $blogData['affiliate_keyword'] }}";amzn_assoc_theme ="light"; amzn_assoc_bg_color ="FFFFFF"; </script><script src="//z-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=GB"></script>
 									</div>
@@ -26,6 +25,9 @@
 									
 								</div>
 								<div class="col-12 col-md-8">
+
+									<amp-img class="col-12" src="{{ URL::to('/') }}/{{ $blogData['image']['path'] }}" width="800" height="400"></amp-img>
+
 									{!! $blogData['content'] !!}
 									
 									<h2 class="text-upper mt-2 mb-2">Related articles</h2>
