ed868b4b3c9e5e644c2569cc5b3487f8a4664e81
Chris Brosnan <cbrosnan00@gmail.com>
2021-01-25 19:19:23 +0000

    refactor controllers

--- resources/views/beverage.blade.php
+++ resources/views/beverage.blade.php
@@ -8,7 +8,7 @@
 				
 				<div id="content-card" class="card">
 					<div class="card-header text-center">
-						<h1>{{ $pageTitle }} (ABV {{ $dataFields['abv'] }}%)</h1>
+						<h1>{{ $bevData['title'] }} (ABV {{ $bevData['abv'] }}%)</h1>
 					</div>
 					<div class="card-body">
 						<div class="container">
@@ -22,7 +22,7 @@
 									</div>
 									
 									<div class="col-12 text-center mb-4 p-0 m-0 dtOnly">
-										<script type="text/javascript">amzn_assoc_ad_type ="responsive_search_widget"; amzn_assoc_tracking_id ="chrisbrosna0e-21"; amzn_assoc_marketplace ="amazon"; amzn_assoc_region ="GB"; amzn_assoc_placement =""; amzn_assoc_search_type = "search_widget";amzn_assoc_width ="300"; amzn_assoc_height ="300"; amzn_assoc_default_search_category =""; amzn_assoc_default_search_key ="<?php echo $dataFields['affiliate_keyword']; ?>";amzn_assoc_theme ="light"; amzn_assoc_bg_color ="FFFFFF"; </script><script src="//z-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=GB"></script>
+										<script type="text/javascript">amzn_assoc_ad_type ="responsive_search_widget"; amzn_assoc_tracking_id ="chrisbrosna0e-21"; amzn_assoc_marketplace ="amazon"; amzn_assoc_region ="GB"; amzn_assoc_placement =""; amzn_assoc_search_type = "search_widget";amzn_assoc_width ="300"; amzn_assoc_height ="300"; amzn_assoc_default_search_category =""; amzn_assoc_default_search_key ="<?php echo $bevData['affiliate_keyword']; ?>";amzn_assoc_theme ="light"; amzn_assoc_bg_color ="FFFFFF"; </script><script src="//z-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=GB"></script>
 									</div>
 									
 									<a class="twitter-timeline dtOnly" href="https://twitter.com/pintglass_ldn?ref_src=twsrc%5Etfw">
@@ -47,13 +47,13 @@
 											</tr>
 											<tr>
 												<td>
-													{{ $dataFields['brewer'] }}
+													{{ $bevData['brewery'] }}
 												</td>
 												<td>
-													{{ $dataFields['first_brewed'] }}
+													{{ $bevData['year_brewed'] }}
 												</td>
 												<td>
-													{{ $dataFields['rev_count'] }}
+													<!--- Count Reviews --->
 												</td>
 											</tr>
 											<tr class="text-upper">
@@ -69,41 +69,37 @@
 											</tr>
 											<tr>
 												<td>
-													{{ $dataFields['place_of_origin'] }}, {{ $dataFields['country_of_origin'] }}
+													{{ $bevData['town_origin'] }}
 												</td>
 												<td>
-													{{ $dataFields['type']['name'] }}
+													{{ $bevData['type'] }}
 												</td>
 												<td class="grey-cell">
-													{{ $dataFields['fav_count'] }} 
+													
 												</td>
 											</tr>
 										</tbody>
 									</table>
 									<br/>
 									<h2 class="text-upper mt-2 mb-2">PintglassLDN review</h2>
-									{{ strip_tags($pageData['content']['rendered']) }} <br/>
+									 <br/>
 									<p class="mt-2 mb-0 pt-2">
 										<span style="font-weight: 500;"><em>PintglassLDN Rating</em></span><br/>
-										@foreach(range(1, $dataFields['rating']) as $n)
+										@foreach(range(1, $bevData['pintglass_rating']) as $n)
 											<i class="fas fa-star yellow-star"></i>
 										@endforeach
-										@foreach(range(1, 10 - $dataFields['rating']) as $n)
+										@foreach(range(1, 5 - $bevData['pintglass_rating']) as $n)
 											<i class="fas fa-star grey-star"></i>
 										@endforeach
-										{{ $dataFields['rating'] }} / 10
+										{{ $bevData['pintglass_rating'] }} / 10
 									</p>
 									<br/>
 									<h2 class="text-upper mt-2 mb-2">User reviews</h2>
 									<div class="mb-4">
 										<!--- Reviews here --->
-										@if($dataFields['rev_count'] == 0)
-											<p>There are no user reviews yet for this beverage. Be the first to <a href="">leave one</a>.</p>
-										@else
-											<p><a href="">Add your own review</a>.</p>
-										@endif
+										<!--- Count Reviews --->
 									</div>
-									<!--<h2 class="text-upper mt-2 mb-2">Others from <em>{{ $dataFields['brewer'] }}</em></h2>
+									<!--<h2 class="text-upper mt-2 mb-2">Others from <em>{{ $bevData['brewery'] }}</em></h2>
 									<div class="mb-4">
 										
 									</div>-->
