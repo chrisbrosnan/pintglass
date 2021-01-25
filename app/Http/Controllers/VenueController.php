2123f1345aea94f39805c96b848256eefdd4d793
Chris Brosnan <cbrosnan00@gmail.com>
2021-01-25 13:56:49 +0000

    rename controller

---
+++ app/Http/Controllers/VenueController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VenueController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        
+    }
+    
+    public function store()
+    {
+        
+    }
+
+    public function show($i)
+    {
+
+    }
+
+    public function edit($i)
+    {
+
+    }
+
+    public function update($i)
+    {
+        
+    }
+
+    public function destroy($i)
+    {
+        
+    }
+}
