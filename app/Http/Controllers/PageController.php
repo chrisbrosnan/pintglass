0823062006cffb477d1852486d8a1ff2f55d26c7
Chris Brosnan <cbrosnan00@gmail.com>
2021-01-26 11:41:33 +0000

    PageController edits to work with new CMS

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -36,7 +36,6 @@ class PageController extends Controller
     {
       $pageData = $this->api->getPageData($slug);
 		  //$pageTitle = $pageData['name'];
-		  $pageSlug = $pageData['slug'];
 	  	$bevData = $this->api->getAllBeverageData(); 
       $blogData = $this->api->getAllBlogData(); 
       $globalData = $this->api->getSingletonData('globals'); 
