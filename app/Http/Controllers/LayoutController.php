a060093a9693da21402b428dd18568db42c126ee
Chris Brosnan <cbrosnan00@gmail.com>
2021-01-26 13:03:09 +0000

    PageController edits to work with new CMS

--- app/Http/Controllers/LayoutController.php
+++ app/Http/Controllers/LayoutController.php
@@ -42,7 +42,7 @@ class LayoutController extends Controller
         {
             $o .= '<a class="col-12 col-md-3" href="https://pintglassldn.com/beverages/' . $i["slug"] . '">
                 <div class="p-0" style="height: 200px; background: url(' . $i["image"]["path"] . '); background-size: cover; background-position: center center;">
-                    <p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">' . $i["name"] . '(ABV '. $i["abv"] .'%)' . '<br/>
+                    <p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">' . $i["name"] . ' (ABV '. $i["abv"] .'%)' . '<br/>
                     <scan class="font-weight-normal" style="font-weight: .5em;">' . $i["brewery"] . ' in ' . $i["town_origin"] . ',' . $i["country_origin"] . '</span></p>
                 </div>
             </a>';
