0594c98f9d7e241a5edb690e809ef0b053e2cbac
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-11 21:25:37 +0000

    Beverages feed on page.blade.php template

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -12,27 +12,31 @@
 					<div class="card-body">
 						<div class="container">
 							<div class="row">
-								<div class="col-12 col-md-4">
-									<amp-img class="col-12" src="" width="300" height="400"></amp-img>
-								</div>
-								<div class="col-12 col-md-8">
 									@if($pageSlug == 'beverages')
-										@php($i = 0)
-										<div class="col-12 row mb-4">
-											<div class="col-12 pb-3 text-center">
-												<h1>Beverages</h1>
+										<div class="col-12 col-md-8">
+											@php($i = 0)
+											<div class="col-12 row mb-4">
+												<div class="col-12 pb-3 text-center">
+													<h1>Beverages</h1>
+												</div>
+												@foreach($bevData as $b)
+													@php($i++)
+													@if($i > 1)
+														<a class="col-12 col-md-3" href="https://pintglassldn.com/beverages/' . $i["slug"] . '">
+															<div class="p-0" style="height: 200px; background: url(' . $i["image"]["path"] . '); background-size: cover; background-position: center center;">
+																<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">' . $i["name"] . ' (ABV '. $i["abv"] .'%)' . '<br/>
+																<scan class="font-weight-normal" style="font-weight: .5em;">' . $i["brewery"] . ' in ' . $i["town_origin"] . ',' . $i["country_origin"] . '</span></p>
+															</div>
+														</a>
+													@endif
+												@endforeach
 											</div>
-											@foreach($bevData as $b)
-												@php($i++)
-												@if($i > 1)
-													<a class="col-12 col-md-3" href="https://pintglassldn.com/beverages/' . $i["slug"] . '">
-														<div class="p-0" style="height: 200px; background: url(' . $i["image"]["path"] . '); background-size: cover; background-position: center center;">
-															<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">' . $i["name"] . ' (ABV '. $i["abv"] .'%)' . '<br/>
-															<scan class="font-weight-normal" style="font-weight: .5em;">' . $i["brewery"] . ' in ' . $i["town_origin"] . ',' . $i["country_origin"] . '</span></p>
-														</div>
-													</a>
-												@endif
-											@endforeach
+										</div>
+									@else 
+										<div class="col-12 col-md-4">
+											<amp-img class="col-12" src="" width="300" height="400"></amp-img>
+										</div>
+										<div class="col-12 col-md-8">
 										</div>
 									@endif
 								</div>
