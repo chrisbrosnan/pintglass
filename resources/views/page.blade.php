f641ac3504ed49f3d389c5ee72b121ed295b7420
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-11 21:35:16 +0000

    Beverages feed on page.blade.php template

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -23,9 +23,9 @@
 													@php($i++)
 													@if($i > 1)
 														<a class="col-12 col-md-3" href="https://pintglassldn.com/beverages/{{ $b['slug'] }}">
-															<div class="p-0" style="height: 200px; background: url('{{ $i['image']['path'] }}'); background-size: cover; background-position: center center;">
-																<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">{{ $i['name'] }} (ABV {{ $i['abv'] }} %)<br/>
-																<scan class="font-weight-normal" style="font-weight: .5em;">{{ $i['brewery'] }} in {{ $i['town_origin'] }}, {{ $i['country_origin'] }}</span></p>
+															<div class="p-0" style="height: 200px; background: url('{{ $b['image']['path'] }}'); background-size: cover; background-position: center center;">
+																<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">{{ $b['name'] }} (ABV {{ $b['abv'] }} %)<br/>
+																<scan class="font-weight-normal" style="font-weight: .5em;">{{ $b['brewery'] }} in {{ $b['town_origin'] }}, {{ $b['country_origin'] }}</span></p>
 															</div>
 														</a>
 													@endif
