0fa7989e19120cf518db346361200d58f4c84e76
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-11 21:37:54 +0000

    Beverages feed on page.blade.php template

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -29,11 +29,21 @@
 												@endforeach
 											</div>
 										</div>
-									@else 
-										<div class="col-12 col-md-4">
-											<amp-img class="col-12" src="" width="300" height="400"></amp-img>
-										</div>
-										<div class="col-12 col-md-8">
+									@elseif($pageSlug == 'blog')
+										<div class="col-12">
+											<div class="col-12 row mb-4">
+												<div class="col-12 pb-3 text-center">
+													<h1>Blog</h1>
+												</div>
+												@foreach($bevData['entries'] as $b)
+													<a class="col-12 col-md-3" href="https://pintglassldn.com/beverages/{{ $b['slug'] }}">
+														<div class="p-0" style="height: 200px; background: url('{{ $b['image']['path'] }}'); background-size: cover; background-position: center center;">
+															<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">{{ $b['name'] }} (ABV {{ $b['abv'] }} %)<br/>
+															<scan class="font-weight-normal" style="font-weight: .5em;">{{ $b['brewery'] }} in {{ $b['town_origin'] }}, {{ $b['country_origin'] }}</span></p>
+														</div>
+													</a>
+												@endforeach
+											</div>
 										</div>
 									@endif
 								</div>
