6b64c9dc0a15b214feae3e3fd3a71b59ab896059
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-11 21:23:15 +0000

    Beverages feed on page.blade.php template

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -18,12 +18,22 @@
 								<div class="col-12 col-md-8">
 									@if($pageSlug == 'beverages')
 										@php($i = 0)
-										@foreach($bevData as $b)
-											@php($i++)
-											@if($i > 1)
-												@php(print_r($b))<br/><br/><br/>
-											@endif
-										@endforeach
+										<div class="col-12 row mb-4">
+											<div class="col-12 pb-3 text-center">
+												<h1>Beverages</h1>
+											</div>
+											@foreach($bevData as $b)
+												@php($i++)
+												@if($i > 1)
+													<a class="col-12 col-md-3" href="https://pintglassldn.com/beverages/' . $i["slug"] . '">
+														<div class="p-0" style="height: 200px; background: url(' . $i["image"]["path"] . '); background-size: cover; background-position: center center;">
+															<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">' . $i["name"] . ' (ABV '. $i["abv"] .'%)' . '<br/>
+															<scan class="font-weight-normal" style="font-weight: .5em;">' . $i["brewery"] . ' in ' . $i["town_origin"] . ',' . $i["country_origin"] . '</span></p>
+														</div>
+													</a>
+												@endif
+											@endforeach
+										</div>
 									@endif
 								</div>
 							</div>
