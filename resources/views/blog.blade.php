815999c2ac42c67edc13212a1345722d85c3711d
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-12 18:38:48 +0000

    BlogController.php data for view

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,7 +7,7 @@
 			<div id="content" class="col-12 col-md-10 m-5">
 				<div class="card">
 					<div class="card-header text-center">
-						<h1></h1>
+						<h1>{{ $blogData[0]['name'] }}</h1>
 					</div>
 					<div class="card-body">
 						<div class="container">
