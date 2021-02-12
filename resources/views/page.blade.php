ab363376922f82171ba62ee34e4b43dd9f91e2b5
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-12 18:33:23 +0000

    BlogController.php data for view

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -33,7 +33,7 @@
 													<h1>Blog</h1>
 												</div>
 												@foreach($blogData['entries'] as $b)
-													<a class="col-12 col-md-3" href="https://pintglassldn.com/beverages/{{ $b['slug'] }}">
+													<a class="col-12 col-md-3" href="https://pintglassldn.com/blog/{{ $b['slug'] }}">
 														<div class="p-0" style="height: 200px; background: url('{{ $b['image']['path'] }}'); background-size: cover; background-position: center center;">
 															<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">{{ $b['name'] }}</p>
 														</div>
