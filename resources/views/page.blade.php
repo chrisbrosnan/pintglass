69c20bfba37930c3bf78b560737eb7ff92e5915c
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-12 18:30:49 +0000

    Blog feed on page.blade.php template

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -35,8 +35,7 @@
 												@foreach($blogData['entries'] as $b)
 													<a class="col-12 col-md-3" href="https://pintglassldn.com/beverages/{{ $b['slug'] }}">
 														<div class="p-0" style="height: 200px; background: url('{{ $b['image']['path'] }}'); background-size: cover; background-position: center center;">
-															<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">{{ $b['name'] }}<br/>
-															<scan class="font-weight-normal" style="font-weight: .5em;"><?php cleanBlogDate($b['date']); ?></span></p>
+															<p class="col-12 text-white font-weight-bold py-2 bg-dark" style="line-height: 1.3em;">{{ $b['name'] }}</p>
 														</div>
 													</a>
 												@endforeach
