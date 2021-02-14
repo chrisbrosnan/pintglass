8d00526a2140981e26ef64943ac856b83c5514ac
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-14 16:31:53 +0000

    Registration page and API user creation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\UserController; 
+use App\Http\Controllers\DataController;
 
 class RegisterController extends Controller
 {
