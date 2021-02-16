2606ae4f4270fcbb290db37016e90585e38c1a90
Chris Brosnan <cbrosnan00@gmail.com>
2021-02-16 00:19:33 +0000

    Reg form

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -146,14 +146,20 @@ class DataController extends Controller
 	{
 
 		return Http::post('https://pintglassldn.com/admin/api/collections/save/users?token=ca2da9e3e1c959fafe7cfb98e94108', [
-			"first_name" => $data['first_name'],
-			"last_name" => $data['last_name'],
-			"email" => $data['email'], 
-			"display_name" => $data['first_name'], 
-			"username" => str_replace(" ", "", $data['username']),
-			"fav_beverage" => $data['favbev'], 
-			"fav_venue" => $data['favbar'], 
-			"bio" => $data['bio']
+			'method': 'post',
+			'headers': { 'Content-Type': 'application/json' },
+			'body': JSON.stringify({
+				'data': {
+					'first_name' => $data['first_name'],
+					'last_name' => $data['last_name'],
+					'email' => $data['email'], 
+					'display_name' => $data['first_name'], 
+					'username' => str_replace(" ", "", $data['username']),
+					'fav_beverage' => $data['favbev'], 
+					'fav_venue' => $data['favbar'], 
+					'bio' => $data['bio']
+				}
+			})
 		]);
 
 		// ); 
